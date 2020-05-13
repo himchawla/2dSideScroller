15e98ffe5c894721f709472c486af80c6e7ddb13
himchawla <himchawla43@gmail.com>
2020-05-13 13:45:02 +1200

    Initial Commit
    Adding the game

---
+++ New Unity Project/Assets/Enemies/EnemyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float attack, baseAttack, health, baseHealth;
+    baseAttack = 20;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
