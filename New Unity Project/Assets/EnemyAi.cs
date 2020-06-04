716e061f2f69cd5725fe9a831714299f4834c6db
himchawla <himchawla43@gmail.com>
2020-06-05 02:22:01 +1200

    some random shiit

---
+++ New Unity Project/Assets/EnemyAi.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+
+public class EnemyAi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed = 200f;
+    public float nextWayPointDistance = 3f;
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+    public Transform enemy;
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, onPathComplete);
+    }
+
+    void onPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path == null)
+            return;
+
+        if(currentWayPoint >=path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        rb.AddForce(force);
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        if(distance<nextWayPointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if(force.x < -0.01f)
+        {
+            enemy.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (force.x > 0.01f)
+        {
+            enemy.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+}
